3efaa99d0e9cc2ea56e66d323d43f707d2727ce9
Irina Be <belecka.irina@gmail.com>
2019-11-30 15:33:25 +0200

    final project start

---
+++ src/com/accenture/finalproject/farm/Farmers.java
@@ -0,0 +1,34 @@
+package com.accenture.finalproject.farm;
+
+public class Farmers {
+	private String name;
+	private int foodAmount;
+	private double budget;
+	private boolean activeStatus;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getFoodAmount() {
+		return foodAmount;
+	}
+
+	public void setFoodAmount(int foodAmount) {
+		this.foodAmount = foodAmount;
+	}
+
+	public double getBudget() {
+		return budget;
+	}
+
+	public void setBudget(double budget) {
+		this.budget = budget;
+	}
+
+	
+}
